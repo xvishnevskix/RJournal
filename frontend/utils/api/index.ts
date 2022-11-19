88c5282acef06df5ce825d92138837d23786b3d0
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2022-11-19 19:23:57 +0400

    Update index.ts

--- frontend/utils/api/index.ts
+++ frontend/utils/api/index.ts
@@ -16,7 +16,7 @@ export const Api = (ctx?: NextPageContext | GetServerSidePropsContext): ApiRetur
     const token = cookies.rtoken;
 
     const instance = axios.create({
-        baseURL: 'http://localhost:7777',
+        baseURL: 'https://rjournalbackend.herokuapp.com',
         headers: {
             Authorization: 'Bearer ' + token,
         },
